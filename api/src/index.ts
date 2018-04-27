5d3abcc0279c83e007ea454a7a6c75ea1419020e
Alexander Johansson <alexander@n1s.se>
2018-04-27 13:40:49 +0100

    🎨 Start echo port from `http`

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,23 @@
+// tslint:disable:no-console
+
 import server from './server';
 
-const {
-  PRISMA_DEBUG,
-  PRISMA_ENDPOINT,
-  PRISMA_SECRET,
-  PORT,
-} = process.env;
+const { PRISMA_DEBUG, PRISMA_ENDPOINT, PRISMA_SECRET, PORT } = process.env;
 
 server({
   PRISMA_ENDPOINT, // the endpoint of the Prisma DB service (value is set in .env)
   PRISMA_SECRET, // taken from database/prisma.yml (value is set in .env)
   PRISMA_DEBUG: !!PRISMA_DEBUG, // log all GraphQL queries & mutations
-
-  // tslint:disable-next-line:no-console
-}).start(() => console.log(`> ✅  GraphQL API Gateway is running on http://localhost:${PORT}`));
+})
+  .start()
+  .then(http =>
+    console.log(
+      `> ✅  GraphQL API Gateway is running on http://localhost:${
+        http.address().port
+      }`,
+    ),
+  )
+  .catch(err => {
+    console.error(`Something went wrong:`, err);
+    process.exit(1);
+  });
