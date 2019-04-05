9c90626be654f6cb35f68152fdc3f66325e8b22d
KATT <alexander@n1s.se>
2019-04-05 16:19:11 -0300

    ⚡ link to kattcorp.com

--- web/pages/about.tsx
+++ web/pages/about.tsx
@@ -1,10 +1,14 @@
-import Head from 'next/head';
 import Layout from '../components/Layout';
+import Logo from '../components/Logo';
 import withOrder from '../lib/withOrder';
 
 export default withOrder(props => (
   <Layout {...props} title="About">
     <h1>About</h1>
-    <a href="https://github.com/KATT/shop">https://github.com/KATT/shop</a>
+    <p>
+      Source: <a href="https://github.com/KATT/shop">https://github.com/KATT/shop</a></p>
+    <hr/>
+    <Logo />
+    <p>Project by <a href="https://kattcorp.com">kattcorp.com</a></p>
   </Layout>
 ));
